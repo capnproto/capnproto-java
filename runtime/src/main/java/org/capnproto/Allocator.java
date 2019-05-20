09eec64941dd991759a555f375e582854ae1d693
David Renshaw <dwrenshaw@gmail.com>
2019-05-20 18:12:19 -0400

    make Allocator public

--- runtime/src/main/java/org/capnproto/Allocator.java
+++ runtime/src/main/java/org/capnproto/Allocator.java
@@ -3,7 +3,7 @@ package org.capnproto;
 /**
  * An object that allocates memory for a Cap'n Proto message as it is being built.
  */
-interface Allocator {
+public interface Allocator {
     /**
      * Allocates a ByteBuffer to be used as a segment in a message. The returned
      * buffer must contain at least `minimumSize` bytes, all of which MUST be
