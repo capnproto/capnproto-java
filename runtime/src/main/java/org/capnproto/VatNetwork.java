e21ba577b5ffb1fe3b20b36220d2e2159b76d139
Vaci Koblizek <vaci@vaci.org>
2020-10-22 13:26:48 +0100

    accept default outgoing message size

--- runtime/src/main/java/org/capnproto/VatNetwork.java
+++ runtime/src/main/java/org/capnproto/VatNetwork.java
@@ -5,6 +5,9 @@ import java.util.concurrent.CompletableFuture;
 public interface VatNetwork<VatId> {
 
      interface Connection {
+         default OutgoingRpcMessage newOutgoingMessage() {
+             return newOutgoingMessage(0);
+         }
          OutgoingRpcMessage newOutgoingMessage(int firstSegmentWordSize);
          CompletableFuture<IncomingRpcMessage> receiveIncomingMessage();
          CompletableFuture<java.lang.Void> onDisconnect();
