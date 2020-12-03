998b569d4cdc889be0de54f7b71b44955bca852f
Vaci Koblizek <vaci@vaci.org>
2020-12-03 12:13:50 +0000

    add ez-rpc

---
+++ runtime-rpc/src/test/java/org/capnproto/EzRpcTest.java
@@ -0,0 +1,31 @@
+package org.capnproto;
+
+
+import org.capnproto.rpctest.Test;
+import org.junit.Assert;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+
+public class EzRpcTest {
+
+    @org.junit.Test
+    public void testBasic() throws Exception {
+        var callCount = new Counter();
+        var address = new InetSocketAddress("localhost", 0);
+        var server = new EzRpcServer(new RpcTestUtil.TestInterfaceImpl(callCount), address);
+        server.start();
+
+        var client = new EzRpcClient(new InetSocketAddress("localhost", server.getPort()));
+
+        var cap = new Test.TestInterface.Client(client.getMain());
+        var request = cap.fooRequest();
+        request.getParams().setI(123);
+        request.getParams().setJ(true);
+
+        var response = client.runUntil(request.send()).join();
+        Assert.assertEquals("foo", response.getX().toString());
+        Assert.assertEquals(1, callCount.value());
+    }
+}
