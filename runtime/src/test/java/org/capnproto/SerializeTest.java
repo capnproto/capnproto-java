89d1c5722ea4d8609351efe14b0d4ea25a53be63
Martin Dindoffer <contact@dindoffer.eu>
2022-05-04 18:40:12 +0200

    Fix integer overflow in computeSerializedSizeInWords

--- runtime/src/test/java/org/capnproto/SerializeTest.java
+++ runtime/src/test/java/org/capnproto/SerializeTest.java
@@ -30,7 +30,14 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SerializeTest {
 
@@ -194,4 +201,13 @@ public class SerializeTest {
             java.nio.channels.Channels.newChannel(new java.io.ByteArrayInputStream(input));
         MessageReader message = Serialize.read(channel);
   }
+
+    @Test
+    @Ignore("Ignored by default because the huge array used in the test results in a long execution")
+    public void computeSerializedSizeInWordsShouldNotOverflowOnLargeSegmentCounts() {
+        ByteBuffer dummySegmentBuffer = ByteBuffer.allocate(0);
+        ByteBuffer[] segments = new ByteBuffer[Integer.MAX_VALUE / 2];
+        Arrays.fill(segments, dummySegmentBuffer);
+        assertThat(Serialize.computeSerializedSizeInWords(segments), is((segments.length * 4L + 4) / Constants.BYTES_PER_WORD));
+    }
 }
