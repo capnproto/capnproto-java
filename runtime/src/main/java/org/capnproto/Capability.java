dee4303ac65e1825d795bdf2359f35f7a3796e7e
Vaci Koblizek <vaci@vaci.org>
2022-12-28 12:21:52 +0000

    reduce the public interface for ClientHook

--- runtime/src/main/java/org/capnproto/Capability.java
+++ runtime/src/main/java/org/capnproto/Capability.java
@@ -6,22 +6,14 @@ import static org.capnproto.ClientHook.NULL_CAPABILITY_BRAND;
 public final class Capability {
 
     public static ClientHook newBrokenCap(String reason) {
-        return newBrokenClient(reason, false, BROKEN_CAPABILITY_BRAND);
-    }
-
-    public static ClientHook newBrokenCap(Throwable exc) {
-        return newBrokenClient(exc, false, BROKEN_CAPABILITY_BRAND);
+        return newBrokenClient(BROKEN_CAPABILITY_BRAND);
     }
 
     public static ClientHook newNullCap() {
-        return newBrokenClient(RpcException.failed("Called null capability"), true, NULL_CAPABILITY_BRAND);
+        return newBrokenClient(NULL_CAPABILITY_BRAND);
     }
 
-    private static ClientHook newBrokenClient(String reason, boolean resolved, Object brand) {
-        return newBrokenClient(RpcException.failed(reason), resolved, brand);
-    }
-
-    private static ClientHook newBrokenClient(Throwable exc, boolean resolved, Object brand) {
+    private static ClientHook newBrokenClient(Object brand) {
         return new ClientHook() {
             @Override
             public Object getBrand() {
